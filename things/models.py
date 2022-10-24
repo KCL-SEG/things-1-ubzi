ff1575e9c839568bfcb57b603ce001d49f900619
Ubaydullah Naik <ubaydullah.naik@kcl.ac.uk>
2022-10-24 11:52:40 +0100

    Refine User Model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,21 @@
+from django.db.models import Model
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing(AbstractUser):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+
+class Thing(Model):
+    name = models.CharField(
+    max_length = 30,
+    unique = True
+    )
+    description = models.CharField(
+    max_length = 120,
+    blank = True
+    )
+    quantity = models.IntegerField(
+    default = 0,
+    validators = [
+    MinValueValidator(0),
+    MaxValueValidator(100),
+    ]
+    )
