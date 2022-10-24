b37eb6c11a5ac0c20f1fbdadaecd7b71ab6e6a99
Ubaydullah Naik <ubaydullah.naik@kcl.ac.uk>
2022-10-24 10:45:29 +0100

    Add Things class

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
+class Things(AbstractUser):
     name = models.TextField()
     description = models.TextField()
     quantity = models.IntegerField()
